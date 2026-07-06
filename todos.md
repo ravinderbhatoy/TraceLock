111a0fb70ae6bcbc0e9185256eb85fae4c624b4f
ravinderbhatoy <ravinderd.bhatoy@gmail.com>
2026-07-06 21:59:28 +0530

    allowed cross-origin requests

--- todos.md
+++ todos.md
@@ -5,4 +5,11 @@
 [x] API Routes
 [x] Serializers
 
+[] drop down options(brands, city, state)
+[] staffProfile
+[] complaint status (fille, under_investigation, recovered, closed)
+
+[] Found request
+
+
 [] frontend (react)
--- tracelock/settings.py
+++ tracelock/settings.py
@@ -39,13 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     "complaints.apps.ComplaintsConfig",
     "users.apps.UsersConfig",
-
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,6 +55,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'tracelock.urls'
 
